1811f3298839a0e9fc6514c14553e5cb78d94d48
Kelly Myers <kelbel0323@yahoo.com>
2015-06-11 23:23:33 -0600

    Add menu icon for responsiveness

--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
 // slide with thumbnails
-/*$(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: "slide",
-    controlNav: "thumbnails"
-  });
-});*/
+// $(window).load(function() {
+//   $('.flexslider').flexslider({
+//     animation: "slide",
+//     controlNav: "thumbnails"
+//   });
+// });
 
+
+// slideshow
 $(window).load(function() {
   $('.flexslider').flexslider({
     animation: "slide"
   });
+
+
+// page transitions
+  var main = $('.main');
+
+  main.css('display', 'none');
+
+  main.fadeIn(500);
+
+  $('a.transition').click(function(event){
+      event.preventDefault();
+      linkLocation = this.href;
+      main.fadeOut(400, redirectPage);
+  });
+
+  function redirectPage() {
+      window.location = linkLocation;
+  }
+
 });
