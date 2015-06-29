898898da829784f799180a062d5e87b1a67ace6c
Kelly Myers <kelbel0323@yahoo.com>
2015-06-28 20:21:43 -0600

    Update slideshow

--- js/script.js
+++ js/script.js
@@ -1,34 +1,68 @@
-// slide with thumbnails
-// $(window).load(function() {
-//   $('.flexslider').flexslider({
-//     animation: "slide",
-//     controlNav: "thumbnails"
-//   });
-// });
 
 
-// slideshow
-$(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: "slide"
-  });
+jQuery(document).ready(function ($) {
+    //Define an array of slideshow transition code
+    var _SlideshowTransitions = [
+      { $Duration: 1200, $Opacity: 2 }
+    ];
+    var options = {
+      $AutoPlay: true,
+      $SlideshowOptions: {
+        $Class: $JssorSlideshowRunner$,
+        $Transitions: _SlideshowTransitions,
+        $TransitionsOrder: 1,
+        $ShowLink: true
+      },
+      $FillMode: 2,
+      $ArrowNavigatorOptions: {
+        $Class: $JssorArrowNavigator$,
+        $ChanceToShow: 2
+      }
+    };
+
+    var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+
+    //responsive code begin
+    function ScaleSlider() {
+        var parentWidth = $('#slider1_container').parent().width();
+        if (parentWidth) {
+            jssor_slider1.$ScaleWidth(parentWidth);
+        }
+        else
+            window.setTimeout(ScaleSlider, 30);
+    }
+    //Scale slider after document ready
+    ScaleSlider();
+
+    //Scale slider while window load/resize/orientationchange.
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    //responsive code end
+
+});
+
+
+
 
 
 // page transitions
-  var main = $('.main');
+  // var main = $('.main');
 
-  main.css('display', 'none');
+  // main.css('display', 'none');
 
-  main.fadeIn(500);
+  // main.fadeIn(500);
 
-  $('a.transition').click(function(event){
-      event.preventDefault();
-      linkLocation = this.href;
-      main.fadeOut(400, redirectPage);
-  });
+  // $('a.transition').click(function(event){
+  //     event.preventDefault();
+  //     linkLocation = this.href;
+  //     main.fadeOut(400, redirectPage);
+  // });
 
-  function redirectPage() {
-      window.location = linkLocation;
-  }
+  // function redirectPage() {
+  //     window.location = linkLocation;
+  // }
 
-});
+// });
+
+// });
